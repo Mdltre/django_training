7509260a80888e6b5e60ed74d4c8942b45f2e562
Mdltre <mjpdelatorre@gmail.com>
2023-04-30 21:43:08 +0800

    Exercise 1: Update-5

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mysite.views import hello
+from exercises.views import hello
 
 urlpatterns = [
     path("hello/", hello),
