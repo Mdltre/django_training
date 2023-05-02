515066839603790c80e6af90bd350cda756b37b6
Mdltre <mjpdelatorre@gmail.com>
2023-05-02 10:47:38 +0800

    Exercise 5: New

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from exercises.models import (
+    Book,
+    Author,
+    Classification
+)
 
-# Register your models here.
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 3
+    
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["title", "author", "publisher"]})
+    ]
+    
+class AuthorAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["title", "author", "publisher"]})
+    ]
+    search_fields=["first_name", "last_name"]
+    
+class PublisherAdmin(admin.ModelAdmin):
+    inlines = [BookInline]   
+    
+admin.site.register(Author)
+admin.site.register(Book,BookAdmin)
+admin.site.register(Classification)
--- exercises/models.py
+++ exercises/models.py
@@ -10,15 +10,29 @@ class Classification(models.Model):
         return self.name
 
 class Author(models.Model):
-    full_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=40, null=True)
+    email = models.EmailField(null=True)
 
     def __str__(self):
-        return self.full_name
+        return f"{self.first_name} {self.last_name}"
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey("exercises.Author", on_delete=models.CASCADE, related_name="exercises")
-    classification = models.ForeignKey("exercises.Classification", on_delete=models.CASCADE, related_name="exercises")
+    publisher = models.ForeignKey("exercises.Publisher", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
+    
+class Publisher(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    address = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=60, null=True)
+    state_province = models.CharField(max_length=30, null=True)
+    country = models.CharField(max_length=50, null=True)
+    website = models.URLField
+    
+    def __str__(self):
+        return self.name
+    
